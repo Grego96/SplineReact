20abf984a7a771df6c0dcc2b4ae23d13a1586f9c
Gregory Hunkeler <gregoryhunkeler@gmail.com>
2024-03-30 12:19:55 -0300

    push spline scene

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Spline from "@splinetool/react-spline";
+
+function App() {
+  return (
+    <Spline scene="https://prod.spline.design/Pu7aSOkw-3gh5V6Z/scene.splinecode" />
+  );
+}
+
+export default App;
